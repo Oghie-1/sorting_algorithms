9c4b30a5007059226f8d7290502e9559b3d949bb
afro <aigbemanuel@gmail.com>
2023-09-23 13:45:58 -0700

    added bubble sort algo

--- sort.h
+++ sort.h
@@ -1,9 +1,15 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 
 
+/* Function to print an array of integers */
+void print_array(const int *array, size_t size);
 
-
-
+/* Bubble Sort function */
 void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
