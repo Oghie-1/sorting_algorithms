de31d1a664cbf97b4926aa7f5f46b38a73a25268
afro <aigbemanuel@gmail.com>
2023-09-29 22:04:56 -0700

    mod sort.h

--- sort.h
+++ sort.h
@@ -9,7 +9,13 @@
 #define UP 0
 #define DOWN 1
 
-/* Define the listint_t type */
+/* Define the listint_t type
+ ** struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ * */
 typedef struct listint_s
 {
     int n;
@@ -49,6 +55,15 @@ void quicksort(int *array, int low, int high, size_t size);
 
 /* Function that initiates the quicksort algorithm*/
 void quick_sort(int *array, size_t size);
+
+
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 /*Function that sorts an array on integers*/
 void shell_sort(int *array, size_t size);
